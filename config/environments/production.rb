2b0c3f24af988e907ff5c35e9176347734eb699a
一宮百合子 <ichinomiyayuriko@mba.local>
2017-10-18 11:38:15 +0900

    heroku設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,19 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+   
+
+   config.action_mailer.default_url_options = { host: 'https://polar-beyond-85821.herokuapp.com/' }
+   ActionMailer::Base.delivery_method = :smtp
+   ActionMailer::Base.smtp_settings =
+    {
+     user_name: "SENDGRID_USERNAME",
+     password: "SENDGRID_PASSWORD",
+     domain: "heroku.com",
+     address: "smtp.sendgrid.net",
+     port: 587,
+     authentication: :plain,
+     enable_starttls_auto: true
+    }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -29,7 +43,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
