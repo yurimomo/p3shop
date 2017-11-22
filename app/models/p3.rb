4203361dfb545fe5151e16ce0a1ee2e84f8baf99
yuriko <yuri715lily_momo@yahoo.co.jp>
2017-11-22 10:40:19 +0900

    validateと投稿画面のcss

--- app/models/p3.rb
+++ app/models/p3.rb
@@ -2,5 +2,6 @@ class P3 < ActiveRecord::Base
  belongs_to :user
  has_many :comments, dependent: :destroy
  mount_uploader :picture , PictureUploader
+ validates :title, :content, :picture, presence: true
 
 end
