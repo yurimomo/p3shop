0a54463074c1bb2d05aa3be220303d71634b1e27
yuriko <yuri715lily_momo@yahoo.co.jp>
2017-12-03 05:35:51 +0900

    devise項目減少とvalidateとログインユーザー同じなら

--- app/models/p3.rb
+++ app/models/p3.rb
@@ -2,6 +2,6 @@ class P3 < ActiveRecord::Base
  belongs_to :user
  has_many :comments, dependent: :destroy
  mount_uploader :picture , PictureUploader
- validates :title, :content, :picture, presence: true
+ validates :title, :content, :picture,:price,  presence: true
 
 end
