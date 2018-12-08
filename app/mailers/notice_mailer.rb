dfb87c6e9d40e2400269eedc9030375b4392cd8f
yuriko <yuri715lily_momo@yahoo.co.jp>
2018-12-08 09:45:08 +0900

    mailer

---
+++ app/mailers/notice_mailer.rb
@@ -0,0 +1,14 @@
+class NoticeMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notice_mailer.sendmail_blog.subject
+  #
+  def sendmail_blog(p3)
+    @p3 = p3
+
+    mail to: "yuri715lily_momo@yahoo.co.jp"
+    subject: 'post is done'
+  end
+end
