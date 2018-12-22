7aa6ca0a73d6bbe0a19aade044df097e3e4ab556
yuriko <yuri715lily_momo@yahoo.co.jp>
2018-12-22 14:16:57 +0900

    dotenv-railsをdevelopmentから外す

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,6 +9,7 @@ Devise.setup do |config|
     config.omniauth :facebook, ENV["FACEBOOK_ID_DEVELOPMENT"], ENV["FACEBOOK_SECRET_DEVELOPMENT"], scope: 'email', display: 'popup', info_fields: 'name, email'
     config.omniauth :twitter, ENV["TWITTER_ID_DEVELOPMENT"], ENV["TWITTER_SECRET_DEVELOPMENT"], scope: 'email', display: 'popup', info_fields: 'name, email'
   end
+  
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
